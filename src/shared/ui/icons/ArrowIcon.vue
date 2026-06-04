1a544ac78734731c02a93cb4f8eca6cda30e2897
Sonia <sofyagorbunova17@gmail.com>
2026-06-04 10:07:42 +0300

    refactor (shared/ui): move inline svg to component

---
+++ src/shared/ui/icons/ArrowIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="32" height="32" viewBox="0 0 24 24" fill="none">
+    <path d="M15 8L19 12M19 12L15 16M19 12H5" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+  </svg>
+</template>
