32420c40b55c1fa8fa230af4110de30a085a6b49
Sonia <sofyagorbunova17@gmail.com>
2026-06-04 10:12:47 +0300

    fix (categories-section): change v-for key from index to unique slug

--- src/widgets/categories-section/ui/CategoriesSection.vue
+++ src/widgets/categories-section/ui/CategoriesSection.vue
@@ -1,34 +1,72 @@
 <script setup>
 import { Typography } from '@/shared/ui/base/Typography';
 import CategoryCard from './CategoryCard.vue';
+import ArrowIcon from '@/shared/ui/icons/ArrowIcon.vue';
+import { useRoute } from 'vue-router';
 
-defineProps({
+const props = defineProps({
   title: String,
-  items: Array
+  items: Array,
+  rootSlug: String
 })
+
+const route = useRoute();
 </script>
 
 <template>
   <section class="category-section">
+    
     <Typography has-line tag="h3" size="xl" weight="bold" class="category-section__title">{{ title }}</Typography>
+
     <div class="category-section__items">
       <CategoryCard
-        v-for="(item, index) in items"
-        v-bind="item"
-        :key="index"
+        v-for="item in items"
+        :key="item.id"
+        :name="item.name"
+        :slug="item.slug"
+        :image-url="item.imageUrl"
+        :parent-slug="item.parentSlug"
       >
       </CategoryCard>
     </div>
+    <RouterLink  v-if="route.name === 'home'" :to="{name: 'catalog', params: {rootCategory: rootSlug}}" class="category-section__link">
+      <Typography weight="bold" size="lg" class="category-section__link-text">
+        {{ `Shop ${props.rootSlug}` }}
+      </Typography>
+      <ArrowIcon class="arrow-icon" />
+    </RouterLink>
   </section>
 </template>
 
 <style scoped>
 .category-section__title {
-  margin-bottom: 70px;
+  margin-bottom: 60px;
 }
 .category-section__items {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(210px, 1fr));
   gap: 50px;
+}
+
+.category-section__link {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 10px;
+  text-transform: capitalize;
+  margin-top: 40px;
+}
+
+.arrow-icon {
+  transition: all 0.3s ease-out;
+}
+
+.category-section__link:hover {
+  .arrow-icon {
+    transform: translateX(5px);
+  }
+  .category-section__link-text {
+    text-decoration: underline;
+  }
 }
 </style>
