2381fb84fc9715f17aeb2822e54be7a8b9af7c9e
Sonia <sofyagorbunova17@gmail.com>
2026-08-13 18:58:46 +0300

    refactor(catalogStore): rename variables and functions

--- src/pages/catalog/model/store.js
+++ src/pages/catalog/model/store.js
@@ -1,11 +1,8 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { productsApi } from '@/shared/api'
-import { useProductStore } from '@/entities/product/model/store'
 
 export const useCatalogStore = defineStore('catalogStore', () => {
-  const productStrore = useProductStore()
-
   const categoryIds = ref([])
 
   const availableFilters = ref({
@@ -25,96 +22,82 @@ export const useCatalogStore = defineStore('catalogStore', () => {
     categoryIds.value = ids
   }
 
-  // const searchQuery = ref('')
-  // const setSearchQuery = query => {
-  //   if (searchQuery.value !== query) {
-  //     searchQuery.value = query
-  //     resetParams()
-  //   }
-  // }
-
-  const setAllColors = colors => {
-    allColors.value = colors
+  const getColorIds = baseColorId => {
+    return allColors.value
+      .filter(
+        color =>
+          color.id === baseColorId || color.base_color_id === baseColorId,
+      )
+      .map(color => color.id)
   }
 
-  const setAvailableFiltersFromProducts = productsData => {
-    const prices = productsData.map(p => p.price)
+  const buildAvailableFilters = productsData => {
+    const prices = productsData.map(product => product.price)
     const minPrice = prices.length ? Math.min(...prices) : 0
     const maxPrice = prices.length ? Math.max(...prices) : 0
 
-    const colors = productsData.flatMap(p => {
-      return p.product_color_variants.map(v => v.colors)
-    })
-
-    const uniqueColors = Array.from(
-      new Map(colors.map(c => [c.id, c])).values(),
+    const availableColorsIds = new Set(
+      productsData.flatMap(product => {
+        return product.product_color_variants.map(variant => variant.colors.id)
+      }),
     )
 
-    const allBaseColorsWithChildren = allColors.value
-      .map(c => {
+    const availableBaseColors = allColors.value
+      .filter(color => color.base_color_id === null)
+      .map(baseColor => {
+        const colorIds = getColorIds(baseColor.id)
+        const isAvailable = colorIds.some(id => availableColorsIds.has(id))
+
+        if (!isAvailable) return null
+
         return {
-          ...c,
-          childrenIds: allColors.value
-            .filter(color => color.id === c.id || c.id === color.base_color_id)
-            .map(childColor => childColor.id),
+          ...baseColor,
+          colorIds,
         }
       })
-      .filter(c => c.base_color_id === null)
-
-    const availableBaseColors = allBaseColorsWithChildren.filter(baseColor => {
-      for (const color of uniqueColors) {
-        if (baseColor.childrenIds.includes(color.id)) {
-          return baseColor
-        }
-      }
-    })
+      .filter(Boolean)
 
     const allSizes = productsData
-      .flatMap(p => {
-        return p.product_color_variants.flatMap(v =>
-          v.products_stock.map(ps => ps.sizes),
+      .flatMap(product => {
+        return product.product_color_variants.flatMap(variant =>
+          variant.products_stock.map(ps => ps.sizes),
         )
       })
       .filter(Boolean)
 
     const uniqueSizes = Array.from(
-      new Map(allSizes.map(s => [s.id, s])).values(),
+      new Map(allSizes.map(size => [size.id, size])).values(),
     ).sort((a, b) => a.id - b.id)
 
     availableFilters.value = {
-      minPrice,
-      maxPrice,
+      minPrice: Math.floor(minPrice),
+      maxPrice: Math.round(maxPrice),
       colors: availableBaseColors,
       sizes: uniqueSizes,
     }
   }
 
-  const loadAvailableFilters = async () => {
+  const fetchAvailableFilters = async () => {
     try {
       if (!allColors.value.length) {
         const colors = await productsApi.getColors()
-        setAllColors(colors)
+        allColors.value = colors
       }
 
-      const rawFiltersData = await productsApi.getFiltersByCategory(
+      const rawFiltersData = await productsApi.getAvailableFilters(
         categoryIds.value,
       )
 
-      setAvailableFiltersFromProducts(rawFiltersData)
+      buildAvailableFilters(rawFiltersData)
     } catch (error) {
       console.error('Error:', error)
     }
-  }
-
-  const loadProducts = async params => {
-    productStrore.fetchProducts(params)
   }
 
   return {
     categoryIds,
     availableFilters,
     setCategoryIds,
-    loadProducts,
-    loadAvailableFilters,
+    fetchAvailableFilters,
   }
 })
